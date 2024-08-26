c455cdbd9110d60809686d7b5af78d6ba70c7dad
Saurav Pundir <114602205+Saurav-pundir@users.noreply.github.com>
2024-08-26 07:01:22 +0530

    Update Phone_Number_Tracker.py

--- Phone_Number_Tracker.py
+++ Phone_Number_Tracker.py
@@ -1,10 +1,17 @@
 import phonenumbers
+from myphone import numbers
 from phonenumbers import timezone , geocoder , carrier
+from opencage importOpenCageGeocode
 number = input("Enter your number with +:")
 phone = phonenumbers.parse(number)
 time = timezone.time_zones_for_number(phone)
 car =carrier.name_for_number(phone , "en")
 reg = geocoder.description_for_valid_number(phone , "en")
+key = "8aa0b2b7327a42fab13c52762d320805"
+geocoder = OpenCageGeocode(key)
+query = str(location)
+result = geocoder.geocode(query)
+print (results)
 print(number)
 print(phone)
 print(time)
